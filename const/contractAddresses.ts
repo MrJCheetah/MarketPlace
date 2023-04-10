938633077fc539692f0e10193e56c84d2ad74eba
mrJxRp <77932181+mrJxRp@users.noreply.github.com>
2023-04-10 14:41:20 +0100

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,17 +2,28 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { Ethereum } from "@thirdweb-dev/chains";
+export const NETWORK = Ethereum;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x6604bd9D7770035f26B4ACeab2C746fdCE166473";
+export const MARKETPLACE_ADDRESS = "0x9Bb01D3549038AE0Ce86FA1D1aF5bF310aDcACB7";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0xFfd9bAddF3f6e427EfAa1A4AEC99131078C1d683";
+  "0x698842107efbba4357fdee60d4ea533d665a68de";
+
+  
+
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://etherscan.io/token/0x698842107efbba4357fdee60d4ea533d665a68de";
+
+
+export const tokenContractAddress =
+  "0x264a7F03c87f004EFD3799F2FcBdf4a1ca477d19";
+export const stakingContractAddress =
+  "0x5B3CE765371372f2551991be8b64fF04a223c5a1";
+  export const derroidContractAddress =
+  "0x955075C2dec2B255262B59FaBFD379F5a5ABab17";
