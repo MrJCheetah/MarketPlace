efc80997348a13343cd56c91f1b3acd8b8d077bf
MrJCheetah <130371933+MrJCheetah@users.noreply.github.com>
2023-04-12 05:41:53 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,10 +38,10 @@ const Home: NextPage = () => {
             <div className={styles.heroBody}>
               <h1 className={styles.heroTitle}>
                 <span className={styles.heroTitleGradient}>
-                  Build NFT Marketplaces
+                  Welcome to the Chediverse
                 </span>
                 <br />
-                faster than ever.
+                Buy - Sell - Stake - Earn
               </h1>
               <p className={styles.heroSubtitle}>
                 <Link
